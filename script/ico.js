17bb3d1f75322670483dd7f0799ca4574c1e1354
Danila <da.kolesnikov.net@gmail.com>
2016-07-25 12:43:16 +0700

    ok

--- script/ico.js
+++ script/ico.js
@@ -2,7 +2,6 @@
 	  // Скрипт появления/скрытия иконок социальных сетей и формы отправки почтовых сообщений.
 
 	  $(".fio").mouseover(function(){$(".socio").removeClass("block_up").addClass("block_down")});
-	  $(".socio").mouseleave(function(){$(".socio").addClass("block_up").removeClass("block_down")});
 
 		$(".email_ico").click(function(event){
 			event.preventDefault();
